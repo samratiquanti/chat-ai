4245aa47c870ee38a16aa3b7db6cc01b327f186f
samratiquanti <65710101+samratiquanti@users.noreply.github.com>
2023-04-05 14:43:34 +0530

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,18 @@
-
+import openai
+import streamlit as st
+from streamlit_chat import message
+import os 
+from dotenv import load_dotenv
+load_dotenv('api_key.env')
+openai.api_key = os.environ.get('API_KEY')
+def generate_response(prompt):
+    completion=openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.6,
+    )
+    message=completion.choices[0].text
+    return message
